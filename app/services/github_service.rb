a30a84761407539a69aef4b2764da52a98891601
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-20 23:41:24 +0000

    automatically backed up by learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -8,7 +8,7 @@ class GithubService
 
   def authenticate!(client_id, client_secret, code)
     response = Faraday.post "https://github.com/login/oauth/access_token",
-        {client_id: client_id, client_secret: client_secret, code: code},
+        {client_id: ENV['GITHUB_CLIENT'], client_secret: ENV['GITHUB_SECRET'], code: code},
         {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
     @access_token = access_hash["access_token"]
@@ -29,5 +29,7 @@ class GithubService
   def create_repo(name)
     Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
   end
+
+
 
 end
